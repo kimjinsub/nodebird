c9adfac8906837294edd38347d95eb08c041cda9
kimjinsub <friday@exciform.com>
2021-12-23 17:55:03 +0900

    first commit

---
+++ prepare/front/components/AppLayout.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+import { Menu, Input, Row, Col } from 'antd';
+import UserProfile from '../components/UserProfile';
+import LoginForm from '../components/LoginForm';
+import styled from 'styled-components';
+
+const SearchInput = styled(Input.Search)`
+    vertical-align: middle;
+`;
+
+const AppLayout = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    return (
+        <div>
+            <Menu mode='horizontal'>
+                <Menu.Item>
+                    <Link href="/"><a>노드버드</a></Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href="/profile"><a>프로필</a></Link>
+                </Menu.Item>
+                <Menu.Item>
+                    <SearchInput enterButton />
+                </Menu.Item>
+                <Menu.Item>
+                    <Link href="/signup"><a>회원가입</a></Link>
+                </Menu.Item>
+            </Menu>
+            <Row gutter={8}>
+                <Col xs={24} md={6}>
+                    {isLoggedIn
+                        ? <UserProfile setIsLoggedIn={setIsLoggedIn} />
+                        : <LoginForm setIsLoggedIn={setIsLoggedIn} />}
+                </Col>
+                <Col xs={24} md={12}>
+                    {children}
+                </Col>
+                <Col xs={24} md={6}>
+                    <a href='https://kimjinsub.github.io/kimjinsub/nodebird'
+                        target="_blank" rel="noreferrer noopener">
+                        Made by kim.js
+                    </a>
+                </Col>
+            </Row>
+        </div>
+    );
+}
+
+AppLayout.propTypes = {
+    children: PropTypes.node.isRequired,
+}
+export default AppLayout;
---
+++ prepare/front/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppLayout from "../components/AppLayOut";
+
+const Home = () => {
+    return (
+        <AppLayout>
+            <div>Hello, Next!</div>
+        </AppLayout>
+    );
+}
+
+export default Home;
