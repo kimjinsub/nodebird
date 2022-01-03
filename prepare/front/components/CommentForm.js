92c8143b11b57180c39cf1b4f7d62820d292ca4e
kimjinsub <friday@exciform.com>
2022-01-03 17:50:39 +0900

    댓글구현하기 4:17

---
+++ prepare/front/components/CommentForm.js
@@ -0,0 +1,12 @@
+import { Form } from 'antd';
+import React from 'react'
+
+const CommentForm = () => {
+    return (
+        <Form>
+            폼
+        </Form>
+    )
+}
+
+export default CommentForm;
--- prepare/front/components/PostForm.js
+++ prepare/front/components/PostForm.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import { Button, Form, Input } from "antd";
 import { useDispatch, useSelector } from "react-redux";
 import { addPost } from "../reducers/post";
@@ -6,13 +6,18 @@ import { addPost } from "../reducers/post";
 const PostForm = () => {
     const { imagePaths } = useSelector((state) => state.post);
     const dispatch = useDispatch();
+    const imageInput = useRef();
     const [text, setText] = useState('');
     const onChangeText = useCallback((e) => {
         setText(e.target.value);
     }, []);
     const onSubmit = useCallback(() => {
         dispatch(addPost);
+        setText('');
     }, []);
+    const onClickImageUpload = useCallback(() => {
+        imageInput.current.click();
+    }, [imageInput.current])
     return (
         <Form
             style={{ margin: '10px 0 20px' }}
@@ -26,8 +31,8 @@ const PostForm = () => {
                 placeholder="어떤 신기한 일이 있었나요?"
             />
             <div>
-                <div type="file" multiple hidden />
-                <Button>이미지 업로드</Button>
+                <div type="file" multiple hidden ref={imageInput} />
+                <Button onClick={onClickImageUpload}>이미지 업로드</Button>
                 <Button type="primary" style={{ float: 'right' }} htmlType="submit">짹짹</Button>
             </div>
             <div>
